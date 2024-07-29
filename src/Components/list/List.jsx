793bb503e5ebe2bd29aa1eac9b2dd06c594c8267
Devasya <devasyarajguru@gmail.com>
2024-07-29 22:00:44 +0530

    feat: Creating list component

---
+++ src/Components/list/List.jsx
@@ -0,0 +1,16 @@
+import ChatList from './chatList/ChatList';
+import './list.css'
+import UserInfo from './userinfo/UserInfo'
+
+
+const List = () =>
+    {
+        return (
+            <div className="list">
+                <UserInfo />
+                <ChatList />
+            </div>
+        )
+    }
+    
+    export default List;
