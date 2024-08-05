960400e297d9052d482ba1acde45f1185fac440e
Devasya <devasyarajguru@gmail.com>
2024-08-05 20:43:35 +0530

    feat: Created user info in Chat section

--- src/Components/chat/Chat.jsx
+++ src/Components/chat/Chat.jsx
@@ -1,9 +1,26 @@
 import './chat.css'
+import avatar from './assets/avatar.png';
 
 const Chat = () =>
 {
     return (
-        <div className="chat">Chat</div>
+        <div className="chat">
+            <div className="top">
+                <div className="user">
+                    <img src={avatar} alt='user'/>
+                    <div className="texts">
+                        <span>Devasya Rajguru</span>
+                        <p>Bio goes here</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    
+                </div>
+            </div>
+            <div className="center"></div>
+            <div className="bottom"></div>
+
+        </div>
     )
 }
 
