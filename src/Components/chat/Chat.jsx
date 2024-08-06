127198fd3cb24bd3fb897a1293c2449c9a3ad949
Devasya <devasyarajguru@gmail.com>
2024-08-06 17:03:40 +0530

    update: Added comments in Chat section

--- src/Components/chat/Chat.jsx
+++ src/Components/chat/Chat.jsx
@@ -4,23 +4,27 @@ import avatar from './assets/avatar.png';
 const Chat = () =>
 {
     return (
+        // chat main container starts
         <div className="chat">
+            {/* top class starts */}
             <div className="top">
+                {/* user class starts */}
                 <div className="user">
                     <img src={avatar} alt='user'/>
-                    <div className="texts">
-                        <span>Devasya Rajguru</span>
+                    <div className="user-texts">
+                        <span>Some User</span>
                         <p>Bio goes here</p>
                     </div>
                 </div>
-                <div className="icons">
-                    
-                </div>
+                {/* user class ends */}
             </div>
+            {/* top class ends */}
+
             <div className="center"></div>
             <div className="bottom"></div>
-
         </div>
+        // chat main container ends
+
     )
 }
 
