a229a3a1f9aa3da0f44d3efd0965f2f37e34a766
Devasya <devasyarajguru@gmail.com>
2024-08-07 20:17:56 +0530

    feat: Added text box , emoji , and icons for chat interface

--- src/Components/chat/Chat.jsx
+++ src/Components/chat/Chat.jsx
@@ -1,5 +1,9 @@
 import './chat.css'
 import avatar from './assets/avatar.png';
+import emoji from './assets/emoji.png';
+import img from './assets/img.png';
+import camera from './assets/camera.png';
+import mic from './assets/mic.png';
 
 const Chat = () =>
 {
@@ -21,7 +25,21 @@ const Chat = () =>
             {/* top class ends */}
 
             <div className="center"></div>
-            <div className="bottom"></div>
+            {/* bottom class starts */}
+            <div className="bottom">
+                <input type='text' placeholder='Type a message...'/>
+                <div className="emoji">
+                    <div className="bottom-icons">
+                        <img src={img} alt=''/>
+                        <img src={camera} alt=''/>
+                        <img src={mic} alt=''/>
+                    </div>
+                    <img src={emoji} />
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
+            {/* bottom class ends */}
+
         </div>
         // chat main container ends
 
