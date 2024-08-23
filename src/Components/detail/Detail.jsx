96dde92cdda0a9a7bdf8f1b5f6b4a8907da5ee57
Devasya <devasyarajguru@gmail.com>
2024-08-23 18:55:08 +0530

    feat: Created detail section for information of chat user

--- src/Components/detail/Detail.jsx
+++ src/Components/detail/Detail.jsx
@@ -1,9 +1,63 @@
 import './detail.css'
+import avatar from './assets/avatar.png';
+import mail from './assets/mail.png'; 
+import download from './assets/download.png'; 
+// import call from './assets/call.png'; 
+// import video from './assets/video.png'; 
+
 
 const Detail = () =>
     {
         return (
-            <div className="detail">Detail </div>
+            // detail class starts
+            <div className="detail">
+                {/*  user class starts */}
+                <div className="detail-user">
+                    <img src={avatar} alt="user"/>
+                    <h2>Jane Doe</h2>
+                    <div className="icons">
+                        <img src={mail} alt='mail' className='mail-icon'/>
+                        {/* <img src={call} alt='more' />
+                        <img src={video} alt='video' /> */}
+                    </div>
+                </div>
+                {/*  user class ends */}
+
+                {/* Info class starts */}
+                <div className="detail-info">
+                    {/* option class starts */}
+                    <div className="detail-option">
+                        <div className="detail-title">
+                            <span>Shared Files</span>
+                        </div>
+                        {/* photos class starts */}
+                        <div className="photos">
+                             {/* photoItem */}
+                            <div className="photoItem">
+                                {/* Photo Detail */}
+                                <div className="photoDetail">
+                                    <img src="https://picsum.photos/200/300" alt="image"/>
+                                    <span>Photo name</span>
+                                </div>
+                                {/* Photo Detail */}
+
+                                <img src={download} />
+                            </div>
+                             {/* photoItem */}
+
+                        </div>
+                        {/* photos class ends */}
+
+                    </div>
+                    {/* option class ends */}
+
+                    <button>Block User</button>
+                </div>
+                {/* Info class ends */}
+
+            </div>
+            // detail class starts
+
         )
     }
     
