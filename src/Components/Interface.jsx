4f0dd3fa742bf086d626c2df80f2c5dd688c168a
Devasya <devasyarajguru@gmail.com>
2024-08-26 19:07:30 +0530

    feat: Condition if user is not signed in

--- src/Components/Interface.jsx
+++ src/Components/Interface.jsx
@@ -2,15 +2,27 @@ import '../CSS/Interface.css'
 import Chat from './chat/Chat';
 import Detail from './detail/Detail';
 import List from './list/List';
+import Login from '../Components/Login'
 
 const Interface = () =>
     {
+        const user = false
         return (
             <>
             <div className="main-interface">
-                <List />
-                <Chat />
-                <Detail />
+               
+                {
+                    user ? 
+                (
+                    <>
+                    <List />
+                    <Chat />
+                    <Detail />
+                    </>
+                ) 
+                    : (<Login/>)
+                }
+               
             </div>
             </>
         )
