1fa2a4e04f255798f829f7a9f16fdca16f7c5450
Devasya <devasyarajguru@gmail.com>
2024-08-27 16:32:18 +0530

    update: Changing the content and adding Link for signup page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,11 @@
 import '../CSS/Login.css'
 import LoginForm from '../assets/loginform2.jpg'
+import { Link } from 'react-router-dom'
 const Login = () =>
     {
         return (
             <>
+            <div className="login-container">
                 <section className="form-section">
     <div className="container">
         <div className="row no-gutters">
@@ -11,12 +13,12 @@ const Login = () =>
                 <img src={LoginForm} className="img-fluid" alt="login form image" />
             </div>
             <div className="col-right">
-                <h1 className="header-text">Welcome To LegalHelpOffice</h1>
+                <h1 className="header-text">Welcome To Legal Help Office</h1>
                 <h4>Sign into your account</h4>
                 <form id="loginForm" method="POST">
                     <div className="form-row">
                         <div className="form-group">
-                            <input type="email" name="mobile" placeholder="Email address" id="EmailInput" className="form-control" required />
+                            <input type="email" name="email" placeholder="Email address" id="EmailInput" className="form-control" required />
                             <div id="EmailError" className="error-message"></div>
                         </div>
                         <div className="form-group">
@@ -27,7 +29,7 @@ const Login = () =>
                             <button id="loginbtn" className="btn" aria-pressed="true"> <span>Login</span></button>
                             <div className="signup-container">
                                 <p className="message">Don't have an account?</p>
-                                <button className="signup-button"><a href="../files/signup.html">Sign Up Now </a></button>
+                                <Link to="/signup"><button className="signup-button">Sign Up Now</button></Link>
                             </div>
                         </div>
                     </div>
@@ -36,6 +38,7 @@ const Login = () =>
         </div>
     </div>
 </section>
+</div>
             </>
         )
     }
