70172223e77ad1b84a3d26725708477e6163d687
Devasya <devasyarajguru@gmail.com>
2024-08-28 19:03:10 +0530

    update: Adding login form comments and to maintain it in the center

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,40 +5,48 @@ const Login = () =>
     {
         return (
             <>
+            {/* Login container starts */}
             <div className="login-container">
+                {/* Form section starts*/}
                 <section className="form-section">
-    <div className="container">
-        <div className="row no-gutters">
-            <div className="col-left">
-                <img src={LoginForm} className="img-fluid" alt="login form image" />
-            </div>
-            <div className="col-right">
-                <h1 className="header-text">Welcome To Legal Help Office</h1>
-                <h4>Sign into your account</h4>
-                <form id="loginForm" method="POST">
-                    <div className="form-row">
-                        <div className="form-group">
-                            <input type="email" name="email" placeholder="Email address" id="EmailInput" className="form-control" required />
-                            <div id="EmailError" className="error-message"></div>
-                        </div>
-                        <div className="form-group">
-                            <input type="password" name="password" placeholder="Password" id="passwordInput" className="form-control" required />
-                            <div id="passwordError" className="error-message"></div>
-                        </div>
-                        <div className="form-group">
-                            <button id="loginbtn" className="btn" aria-pressed="true"> <span>Login</span></button>
-                            <div className="signup-container">
-                                <p className="message">Don't have an account?</p>
-                                <Link to="/signup"><button className="signup-button">Sign Up Now</button></Link>
+                    {/* Container starts */}
+                    <div className="container">
+                        <div className="row no-gutters">
+                            <div className="col-left">
+                                <img src={LoginForm} className="img-fluid" alt="login form image" />
                             </div>
+                        <div className="col-right">
+                            <h1 className="header-text">Welcome To Legal Help Office</h1>
+                            <h4>Sign into your account</h4>
+                            <form id="loginForm" method="POST">
+                                <div className="form-row">
+                                    <div className="form-group">
+                                        <input type="email" name="email" placeholder="Email address" id="EmailInput" className="form-control" required />
+                                    <div id="EmailError" className="error-message"></div>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="password" name="password" placeholder="Password" id="passwordInput" className="form-control" required />
+                                    <div id="passwordError" className="error-message"></div>
+                                    </div>
+                                    <div className="form-group">
+                                    <button id="loginbtn" className="btn" aria-pressed="true"> <span>Login</span></button>
+                                    <div className="signup-container">
+                                        <p className="message">Don't have an account?</p>
+                                        <Link to="/signup"><button className="signup-button">Sign Up Now</button></Link>
+                                    </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                         </div>
                     </div>
-                </form>
+                    {/* Container ends */}
+
+                </section>
+                {/* Form section ends*/}
+
             </div>
-        </div>
-    </div>
-</section>
-</div>
+            {/* Login container ends */}
             </>
         )
     }
