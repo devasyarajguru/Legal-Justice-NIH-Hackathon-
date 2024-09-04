949579921bbf0edf9b0c2fe8b0d78f4a9dfb9d88
Devasya <devasyarajguru@gmail.com>
2024-09-04 21:36:47 +0530

    update: uploading functionality using promise for image

--- src/Components/lib/upload.js
+++ src/Components/lib/upload.js
@@ -1,30 +1,30 @@
-const upload = async() =>
-{
-    const storageRef = ref(storage, 'images/rivers.jpg');
+import { getDownloadURL , ref , uploadBytesResumable } from "firebase/storage";
+import {storage} from "./firebase";
+
+const upload = async(file) =>
+{ 
+    const date = new Date()
+    const storageRef = ref(storage, `images/${date + file.name}`);
 
     const uploadTask = uploadBytesResumable(storageRef, file);
 
-    uploadTask.on('state_changed', 
+    return new Promise((resolve,reject)=>
+    {
+      uploadTask.on('state_changed', 
         (snapshot) => {
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-      case 'paused':
-        console.log('Upload is paused');
-        break;
-      case 'running':
-        console.log('Upload is running');
-        break;
-    }
-  }, 
-  (error) => {
-  }, 
-  () => {
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log('File available at', downloadURL);
-    });
-  }
-);
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+            }, 
+            (error) => {
+              reject("Something went wrong" + error.message)
+            }, 
+            () => {
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                resolve(downloadURL);
+              });
+            }
+          );
+        })
 }
 
 export default upload;
