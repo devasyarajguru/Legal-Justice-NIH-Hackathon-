517d87070eb7e46804a4fa70eeeaa3e0e0853082
Devasya <devasyarajguru@gmail.com>
2024-09-23 19:28:00 +0530

    feat: Creating onAuthState for maintaing logging of user

--- src/Components/Interface.jsx
+++ src/Components/Interface.jsx
@@ -4,10 +4,28 @@ import Detail from './detail/Detail';
 import List from './list/List';
 import Login from '../Components/Login'
 import Notification from '../Components/Notification'
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import {auth} from './lib/firebase'
+
 
 const Interface = () =>
     {
         const user = false
+
+        useEffect(() =>
+        {
+            const unSub = onAuthStateChanged(auth,(user)=> // onAuthStateChanged is a Firebase Auth listener that checks the current authentication state (whether a user is logged in or not)
+            {
+                console.log(user)
+            })
+
+            return () =>
+            {
+                unSub(); // returns this function so that onAuthStateChanged is unsubscribed , when the component unmounts avoiding potential memory leaks from keeping unused listeners active.
+            };
+        },[])
+
         return (
             <>
             <div className="main-interface">  
