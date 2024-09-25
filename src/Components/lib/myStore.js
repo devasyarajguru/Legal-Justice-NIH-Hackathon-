fc59763065e2e178f3087f4b904302991d6d13fb
Devasya <devasyarajguru@gmail.com>
2024-09-25 21:51:35 +0530

    feat: Creating state managment using zustand tools

---
+++ src/Components/lib/myStore.js
@@ -0,0 +1,34 @@
+import { doc, getDoc } from "firebase/firestore";
+import { create } from "zustand";
+
+ export const userStore = create((set) => (
+{
+    currentUser:null,
+    isLoading: true,
+    fetchUserInfo: async(uid) =>
+    {
+        if(!uid) return set({currentUser: null , isLoading: false});
+
+        try
+        {
+            const docRef = doc(db, "users", uid);
+            const docSnap = await getDoc(docRef);
+
+            if(docSnap.exists())
+            {
+                set({currentUser: docSnap.data() , isLoading: false})
+            }
+            
+            else
+            {
+                set({currentUser: null , isLoading: false})
+            }
+
+        }
+
+        catch(err)
+        {
+            return set({currentUser: null , isLoading: false});
+        }
+    }
+}))
