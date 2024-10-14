2c8c7a979234342d8d5cfb59a00a3c3ea9599ea2
Devasya <devasyarajguru@gmail.com>
2024-10-14 19:28:12 +0530

    update: Making the avatar and name dynamic from firebase

--- src/Components/list/userinfo/UserInfo.jsx
+++ src/Components/list/userinfo/UserInfo.jsx
@@ -3,16 +3,19 @@ import avatar from './assets/avatar.png';
 import video from './assets/video.png';
 import edit from './assets/edit.png';
 import more from './assets/more.png';
+import { useUserStore } from '../../lib/userStore';
 
 const UserInfo = () =>
     {
+        const {currentUser} = useUserStore();
+
         return (
             // UserInfo Starts
         <div className="userInfo">
                 {/* user class starts */}
             <div className="user-chatList">
-                <img src={avatar} alt="user" className='user-img' />
-                <h2>Devasya Rajguru</h2>
+                <img src={currentUser.avatar || avatar} alt="user" className='user-img' />
+                <h2>{currentUser.username}</h2>
             </div>
                 {/* user class ends */}
 
