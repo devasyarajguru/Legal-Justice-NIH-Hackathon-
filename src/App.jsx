ec8517b1ecc9e8f9f9f6d965ede5dbef504b2375
Devasya <devasyarajguru@gmail.com>
2024-11-11 19:21:38 +0530

    update: Changing proper path of UnderConstruction

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import UnderConstruction from './components/UnderConstruction';
+import UnderConstruction from '../src/Components/UnderConstruction';
 
 function App() {
   return <UnderConstruction />;
