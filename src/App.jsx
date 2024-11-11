a4daee38a407a674b221ed705effaddba3568c1e
Devasya <devasyarajguru@gmail.com>
2024-11-11 19:04:05 +0530

    update: Replacing the under construction component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import './App.css'
-import { Route, Routes } from "react-router-dom";
-import Sidebar from './Components/Sidebar';
-import Home from './Components/Home';
-import Interface from './Components/Interface';
-import Chatbot from './Components/Chatbot';
-import Awareness from './Components/Awareness';
-import Login from './Components/Login';
-import SignUp from './Components/SignUp';
+import UnderConstruction from './components/UnderConstruction';
 
 function App() {
-
-  return (
-    <>
-    {/* Amma Sidebar and App container class flex css ma che etle badha svgs home jsx ma differ thayela che */}
-    <div className='app-container'>
-      <Sidebar />
-      <div className='main-content'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/interface' element={<Interface />} />
-        <Route path='/chatbot' element={<Chatbot />} />
-        <Route path='/awareness' element={<Awareness />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-      </Routes>
-      </div>
-    </div>
-      
-    </>
-  )
+  return <UnderConstruction />;
 }
 
-export default App
+export default App;
